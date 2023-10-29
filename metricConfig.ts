3b048804267246de883fcf806d86b535490aca7b
Protyay Banerjee <protyay91banerjee@gmail.com>
2023-10-29 22:44:16 +0530

    Create metricConfig.ts
    Metric type information

---
+++ metricConfig.ts
@@ -0,0 +1,75 @@
+export type MetricInfo = {
+  metricName: string;
+  metricUnit: Unit;
+  metricStats: MetricStatistic;
+  alarmFriendlyName: string;
+  alarmConfig?: {
+    [Severity.SEV2]?: {
+      threshold: number;
+      comparisonOperator: ComparisonOperator;
+      treatMissingDataOverride: TreatMissingData;
+    };
+    [Severity.SEV3]: {
+      threshold: number;
+      comparisonOperator: ComparisonOperator;
+      treatMissingDataOverride: TreatMissingData;
+    };
+  };
+};
+
+function buildMetricAndAlarm(applicableMetrics: MetricInfo[]): CustomMetric[] {
+  const metrics: CustomMetric[] = [];
+  applicableMetrics.forEach((applicationMetric) => {
+    const metric = new Metric({
+      namespace: ServiceConstants.SERVICE_NAME,
+      metricName: applicationMetric.metricName,
+      unit: applicationMetric.metricUnit,
+      statistic: applicationMetric.metricStats,
+      dimensionsMap: {
+        Operation: MonitoringConstants.OPERATION_NAME,
+      },
+    });
+
+    const metricItem = {
+      metric,
+      alarmFriendlyName: applicationMetric.alarmFriendlyName,
+      addAlarm: {},
+    };
+
+    if (applicationMetric.alarmConfig && applicationMetric.alarmConfig[Severity.SEV3]) {
+      metricItem.addAlarm = {
+        Warning: applicationMetric.alarmConfig[Severity.SEV3],
+      };
+    }
+
+    if (applicationMetric.alarmConfig && applicationMetric.alarmConfig[Severity.SEV2]) {
+      metricItem.addAlarm = {
+        ...metricItem.addAlarm,
+        Critical: applicationMetric.alarmConfig[Severity.SEV2],
+      };
+    }
+
+    metrics.push(metricItem);
+  });
+  return metrics;
+}
+
+/**
+ * Helper function to build the metric group
+ * that will be rendered as a single widget in the dashboard.
+ * @param system the system object from the enum
+ */
+export function buildMetricGroup(system: DownstreamSystems): CustomMetricGroup[] {
+  const metricGroup: CustomMetricGroup[] = [];
+  const applicableMetrics = dependentSystemsMetrics[system];
+  const metricWithAlarms = buildMetricAndAlarm(applicableMetrics);
+  metricGroup.push({
+    metrics: metricWithAlarms,
+    title: `${system}-Metrics`,
+    graphWidgetType: GraphWidgetType.LINE,
+    graphWidgetAxis: {
+      label: `${system}-Metrics`,
+    },
+  });
+  return metricGroup;
+}
